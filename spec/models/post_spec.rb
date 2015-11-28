f71366b1b6fda2ec9315656fde0afbd75b1b24ff
Dan Kang <dtkang@gmail.com>
2015-11-28 19:37:07 +0000

    Checkpoint 35 completed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,9 +3,16 @@ include RandomData
 
 RSpec.describe Post, type: :model do
   let(:topic) { Topic.create!(name: Random.random_sentence, description: RandomData.random_paragraph) }
-  let(:post) { topic.posts.create!(title: "New Post Title", body: "New Post Body") }
+  let(:post) { topic.posts.create!(title: "New Post Title", body: "New Post Body       ") }
   
   it { should belong_to(:topic) }
+  
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:topic) }
+  
+  it { should validate_length_of(:title).is_at_least(5) }
+  it { should validate_length_of(:body).is_at_least(20) }
   
   describe "attributes" do
     it "should respond to title" do
