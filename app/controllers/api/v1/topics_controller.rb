6dd1db08cddbeeef20da6e233b56c4755f368ef9
Dan Kang <dtkang@gmail.com>
2016-01-02 03:29:27 +0000

    Assignment 48: first commit

--- app/controllers/api/v1/topics_controller.rb
+++ app/controllers/api/v1/topics_controller.rb
@@ -9,6 +9,11 @@ class Api::V1::TopicsController < Api::V1::BaseController
   
   def show
     topic = Topic.find(params[:id])
-    render json: topic.to_json, status: 200
+    posts = topic.posts
+    render :json => {
+      topic.to_json,
+      posts.to_json,
+      status: 200
+    }
   end
 end
